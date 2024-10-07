bf225a664aa74d31abcb41cf0b1fc5d8dd122b90
SandyHerrera04 <133033654+SandyHerrera04@users.noreply.github.com>
2024-10-07 11:47:16 -0600

    Create List.jsx

---
+++ src/pages/list/List.jsx
@@ -0,0 +1,27 @@
+import "./list.scss"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Navbar from "../../components/navbar/Navbar"
+import Datatable from "../../components/datatable/Datatable"
+import DatatableProducts from "../../components/datatableProducts/DatatableProducts"
+import DatatableOrders from "../../components/datatableOrders/DatatableOrders"
+
+const List = ({ type }) => {
+  return (
+    <div className="list">
+      <Sidebar />
+      <div className="listContainer">
+        <Navbar />
+        {/* Condicional para decidir qué tabla cargar */}
+        {type === "products" ? (
+          <DatatableProducts />
+        ) : type === "orders" ? (
+          <DatatableOrders />
+        ) : (
+          <Datatable /> // Usuarios por defecto
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default List;
