d4857a8098ca85eb407f5c1152b5d7c2c21b211a
SandyHerrera04 <133033654+SandyHerrera04@users.noreply.github.com>
2024-10-20 13:43:23 -0600

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,21 +3,8 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-/*
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: "tutorial-5a2cf.firebaseapp.com",
-  projectId: "tutorial-5a2cf",
-  storageBucket: "tutorial-5a2cf.appspot.com",
-  messagingSenderId: "585126334212",
-  appId: "1:585126334212:web:8539eafc56885e1b4c4a51"
-};
-
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth();
-export const storage = getStorage(app);
-*/
+// Verificar la clave de entorno
+console.log("Firebase API Key:", process.env.REACT_APP_FIREBASE_KEY);
 
 // Import the functions you need from the SDKs you need
 //import { initializeApp } from "firebase/app";
@@ -28,14 +15,23 @@ export const storage = getStorage(app);
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "zazil-daa11.firebaseapp.com",
+  databaseURL: "https://zazil-daa11-default-rtdb.firebaseio.com",
   projectId: "zazil-daa11",
   storageBucket: "zazil-daa11.appspot.com",
   messagingSenderId: "107009042079",
   appId: "1:107009042079:web:21a6cf88e62d716317e4aa"
 };
 
+// Inicialización de Firebase con manejo de errores
+let app;
+try {
+  app = initializeApp(firebaseConfig);
+  console.log("Firebase initialized:", app);
+} catch (error) {
+  console.error("Error initializing Firebase:", error);
+}
+
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth();
 export const storage = getStorage(app);
