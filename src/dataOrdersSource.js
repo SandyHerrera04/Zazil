ce5d67d3fe743ebeb7f865b56ed1906313e83c96
SandyHerrera04 <133033654+SandyHerrera04@users.noreply.github.com>
2024-10-20 14:34:42 -0600

    Update dataOrdersSource.js

--- src/dataOrdersSource.js
+++ src/dataOrdersSource.js
@@ -1,62 +1,38 @@
+// Definición de las columnas para la tabla de órdenes
 export const orderColumns = [
-    { field: "id", headerName: "ID", width: 70 },
-
-    {
-        field: "product",
-        headerName: "Producto",
-        width: 230,
-        renderCell: (params) => {
-          return (
-            <div className="cellWithImg">
-              <img className="cellImg" src={params.row.img} alt="avatar" />
-              {params.row.product}
-            </div>
-          );
-        },
-      },
-
-    {
-      field: "user",
-      headerName: "User",
-      width: 230,
-      renderCell: (params) => {
-        return (
-          <div className="cellWithImg">
-            <img className="cellImg" src={params.row.img} alt="avatar" />
-            {params.row.username}
-          </div>
-        );
-      },
+  { 
+    field: "id", 
+    headerName: "ID", 
+    width: 70 // Ancho de la columna ID
+  },
+  {
+    field: "createdAt",
+    headerName: "Fecha",
+    width: 150, // Ancho de la columna Fecha
+    renderCell: (params) => {
+      // Obtiene la fecha de creación del objeto params
+      const date = params.row.createdAt 
+        ? (params.row.createdAt.toDate ? params.row.createdAt.toDate() : new Date(params.row.createdAt))
+        : null;  // Maneja el caso donde createdAt es indefinido
+      
+      // Formatea la fecha a una cadena legible en español
+      const formattedDate = date ? date.toLocaleDateString('es-ES') : 'Fecha no disponible'; // Mensaje de reserva
+      return formattedDate;
     },
-  
-    {
-      field: "date",
-      headerName: "Fecha",
-      width: 150,
-    },
-  
-    {
-      field: "price",
-      headerName: "Precio",
-      width: 150,
-    },
-  
-    {
-      field: "paymentMethod",
-      headerName: "Método de Pago",
-      width: 230,
-    },
-  
-    {
-      field: "status",
-      headerName: "Status",
-      width: 160,
-      renderCell: (params) => {
-        return (
-          <div className={`cellWithStatus ${params.row.status}`}>
-            {params.row.status}
-          </div>
-        );
-      },
-    },
-  ];
+  },
+  {
+    field: "amount",
+    headerName: "Precio",
+    width: 150, // Ancho de la columna Precio
+  },
+  {
+    field: "currency",
+    headerName: "Tipo de moneda",
+    width: 150, // Ancho de la columna Tipo de moneda
+  },
+  {
+    field: "status",
+    headerName: "Estatus",
+    width: 230, // Ancho de la columna Estatus
+  },
+];
