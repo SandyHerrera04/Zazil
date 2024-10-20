40412c40283ac2da8fd63d84abf006fd60feca96
SandyHerrera04 <133033654+SandyHerrera04@users.noreply.github.com>
2024-10-20 14:20:38 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
+// Importaciones necesarias de React y ReactDOM
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthContextProvider } from "./context/AuthContext";
 import { DarkModeContextProvider } from "./context/darkModeContext";
 
+// Renderiza la aplicación sin React.StrictMode, envolviendo la aplicación en los proveedores de contexto
 ReactDOM.render(
-  <React.StrictMode>
-    <DarkModeContextProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </DarkModeContextProvider>
-  </React.StrictMode>,
+  <DarkModeContextProvider>
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
+  </DarkModeContextProvider>,
   document.getElementById("root")
 );
